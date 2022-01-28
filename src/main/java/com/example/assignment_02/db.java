be8fc74cfc35694483699a482a214249541559d7
payalj2000 <payalj152000@gmail.com>
2022-01-28 17:33:45 +0530

    added

--- src/main/java/com/example/assignment_02/db.java
+++ src/main/java/com/example/assignment_02/db.java
@@ -16,10 +16,10 @@ import java.sql.SQLException;
 public class db {
     public Connection init() throws ClassNotFoundException, SQLException{
         
-        String dbURL ="jdbc:postgresql://ec2-3-216-113-109.compute-1.amazonaws.com:5432/";
-        String dbName = "dmt8qlgq6aj62";
-        String dbUsername = "oopbehpbtxzziu";
-        String dbPassword = "6ae6a26fd44588be28bf45a0f08a99ba2c4bdb6280d9c4bf0e9b88d995f4ab42";
+        String dbURL ="";
+        String dbName = "";
+        String dbUsername = "";
+        String dbPassword = "";
         DriverManager.registerDriver(new org.postgresql.Driver());
         Connection con = DriverManager.getConnection(dbURL + dbName,dbUsername,dbPassword);
             
